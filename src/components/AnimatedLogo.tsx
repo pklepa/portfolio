e8383c4d4271a85912012ec08a3a7cd8dce21535
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-25 23:54:40 +0000

    Refactor component and insert rotation animation for the background

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from 'react';
+import { motion } from 'framer-motion';
 
 import styles from '../styles/components/AnimatedLogo.module.scss';
 
@@ -7,8 +8,31 @@ interface Props {}
 function AnimatedLogo(): ReactElement<Props> {
   return (
     <div className={styles.logoContainer}>
-      <h1>pedro klepa</h1>
-      <div className={styles.gradientBackground} />
+      <h1>pk</h1>
+
+      <motion.div
+        animate={{
+          scale: [1, 1, 1.5, 1.5, 1],
+          opacity: [0.2, 1, 1, 1, 1],
+
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['10%', '10%', '50%', '40%', '30%', '10%'],
+        }}
+        transition={{ duration: 2 }}
+        className={styles.solidBackground}
+      />
+
+      <motion.div
+        animate={{
+          opacity: [0.2, 1, 1, 1, 1],
+
+          scale: [1, 1, 1.5, 1.5, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['10%', '10%', '50%', '40%', '30%', '10%'],
+        }}
+        transition={{ duration: 2 }}
+        className={styles.gradientBackground}
+      />
     </div>
   );
 }
