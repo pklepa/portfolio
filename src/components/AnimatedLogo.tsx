2b7044b563910f4d690e551d2754be33f5983242
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-26 00:39:30 +0000

    Add some complexity to the animation with useAnimation

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -1,39 +1,70 @@
-import { ReactElement } from 'react';
-import { motion } from 'framer-motion';
+import { ReactElement, useEffect, useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 
 import styles from '../styles/components/AnimatedLogo.module.scss';
 
 interface Props {}
 
 function AnimatedLogo(): ReactElement<Props> {
+  let solidBackgroundAnim = useAnimation();
+  let gradientBackgroundAnim = useAnimation();
+  const [toggle, setToggle] = useState(false);
+
+  async function solidBackgroundAnimation() {
+    await solidBackgroundAnim.start({
+      scale: [1, 1, 1.5, 1.5, 1],
+      opacity: [0.2, 1, 1, 1, 1],
+
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ['10%', '10%', '50%', '40%', '30%', '8px'],
+    });
+
+    await solidBackgroundAnim.start({
+      width: '200px',
+      transition: { duration: 1 },
+    });
+  }
+
+  async function grandientBackgroundAnimation() {
+    await gradientBackgroundAnim.start({
+      scale: [1, 1, 1.5, 1.5, 1],
+      opacity: [0.2, 1, 1, 1, 1],
+
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ['10%', '10%', '50%', '40%', '30%', '8px'],
+    });
+
+    await gradientBackgroundAnim.start({
+      width: '208px',
+      transition: { duration: 1 },
+    });
+  }
+
+  useEffect(() => {
+    solidBackgroundAnimation();
+    grandientBackgroundAnimation();
+  }, []);
+
   return (
-    <div className={styles.logoContainer}>
+    <motion.div
+      animate={{ width: '200px' }}
+      transition={{ duration: 1, delay: 2 }}
+      className={styles.logoContainer}
+    >
       <h1>pk</h1>
 
       <motion.div
-        animate={{
-          scale: [1, 1, 1.5, 1.5, 1],
-          opacity: [0.2, 1, 1, 1, 1],
-
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ['10%', '10%', '50%', '40%', '30%', '10%'],
-        }}
+        animate={solidBackgroundAnim}
         transition={{ duration: 2 }}
         className={styles.solidBackground}
       />
 
       <motion.div
-        animate={{
-          opacity: [0.2, 1, 1, 1, 1],
-
-          scale: [1, 1, 1.5, 1.5, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ['10%', '10%', '50%', '40%', '30%', '10%'],
-        }}
+        animate={gradientBackgroundAnim}
         transition={{ duration: 2 }}
         className={styles.gradientBackground}
       />
-    </div>
+    </motion.div>
   );
 }
 
