90f6ad37ff3de91a1971bbefc144f9620e0ee9f2
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-26 22:44:51 +0000

    Prepare skill cards grid

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -10,6 +10,23 @@ function SkillsSection({}: Props): ReactElement {
   return (
     <section className={styles.container}>
       <SectionHeader title="Tools & Skills" alignRight={false} />
+
+      <div className={styles.cardsGrid}>
+        <div className={styles.cardWrapper}>
+          <h1>Front-end</h1>
+          <div className={styles.card}></div>
+        </div>
+
+        <div className={styles.cardWrapper}>
+          <h1>Back-end</h1>
+          <div className={styles.card}></div>
+        </div>
+
+        <div className={styles.cardWrapper}>
+          <h1>Miscellaneous</h1>
+          <div className={styles.card}></div>
+        </div>
+      </div>
     </section>
   );
 }
