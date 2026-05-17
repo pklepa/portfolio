0cc6086323abe4cb0085ad50623018630ca23f11
Pedro Klepa <devpklepa@certara.co.uk>
2026-05-17 20:36:40 +0100

    Update SPRING_BOUNCY animation parameters for improved responsiveness; simplify layout in App component by removing unnecessary padding

--- src/constants/animations.ts
+++ src/constants/animations.ts
@@ -1,3 +1,3 @@
 export const SPRING_SMOOTH = { damping: 10, stiffness: 50, restDelta: 0.011 };
 
-export const SPRING_BOUNCY = { damping: 5, stiffness: 120, restDelta: 0.01 };
+export const SPRING_BOUNCY = { damping: 8, stiffness: 120, restDelta: 0.01 };
